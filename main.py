19db7ac67c439dc1db070d237f2063804737d3f1
QAMikaelle <mikaellequalidade@gmail.com>
2025-03-20 14:45:39 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-notifications")
+service = Service("./chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get('https://www.hml.lector.live/testesautomatizados/subscribe/login')
+time.sleep(10)
